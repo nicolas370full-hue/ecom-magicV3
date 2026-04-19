710a9d98c4323d9c53a76a72129e3bbdb3f89afe
nicolas370full-hue <nicolas.370full@gmail.com>
2026-04-19 11:15:13 -0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['fal.media', 'storage.googleapis.com'],
-  },
-}
-
-module.exports = nextConfig
