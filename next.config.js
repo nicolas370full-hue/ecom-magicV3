e36be9498654a8cda8d70e721eb7634c88c1a85b
nicolas370full-hue <nicolas.370full@gmail.com>
2026-04-19 03:24:39 -0400

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['fal.media', 'storage.googleapis.com'],
+  },
+}
+
+module.exports = nextConfig
